d24ba0cb70e3e4abdb886529ee05d88448d49606
PAN ZHI QI <90165904+1201200250PZQ@users.noreply.github.com>
2021-09-13 09:44:04 +0800

    Add files via upload

---
+++ Lab 2.1.py
@@ -0,0 +1,7 @@
+# Student ID : 1201200250
+# Student Name : PAN ZHI QI
+
+kilo = float(input ("Please enter the value of kilogram : "))
+gram = kilo * 1000
+
+print("For {:.2f} kilogram is equivalent to {:.2f} gram".format(kilo,gram))
---
+++ Lab 2.2.py
@@ -0,0 +1,15 @@
+# Student ID : 1201200250
+# Student Name : PAN ZHI QI
+
+import math
+
+FT= 0.3048 #constant value, the value doesn't change, name in Capital letter
+
+feet = float(input("Please enter the value for the number of feet : "))
+
+meters = FT * feet
+
+print("{:.2f} feet is equavalent to {:.2f} meters".format(feet,meters))
+
+print(math.pi)
+print(math.e)
